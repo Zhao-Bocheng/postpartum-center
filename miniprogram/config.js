366ff9734bdf43afeb9d0f7df7731f0c0869f398
TCENT\bocheng.zhao <bocheng.zhao@ly.com>
2022-02-24 18:14:59 +0800

    添加项目使用到的配置信息文件config.js

---
+++ miniprogram/config.js
@@ -0,0 +1,23 @@
+/**
+ * 小程序配置文件
+ */
+
+// const host = '14592619.qcloud.la'
+
+const config = {
+  // 测试的请求地址，用于测试会话
+  // requestUrl: 'https://mp.weixin.qq.com',
+  // host,
+
+  // 云开发环境 ID
+  envId: 'cloud1-9g50mutze1d19816',
+  envList: [{"envId":"cloud1-9g50mutze1d19816","alias":"cloud1"},{"envId":"cloud2-8gf13u9f5e58d9bd","alias":"cloud2"}],
+  // envId: 'release-b86096',
+  // envId: 'test-f0b102',
+
+  // 云开发-存储 示例文件的文件 ID
+  // demoImageFileId: 'cloud://release-b86096.7265-release-b86096-1258211818/demo.jpg',
+  // demoVideoFileId: 'cloud://release-b86096.7265-release-b86096/demo.mp4',
+}
+
+export default config;
