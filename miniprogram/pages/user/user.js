e335dc72a0f9914a17fc7afa5c97600d6ddb8034
TCENT\bocheng.zhao <bocheng.zhao@ly.com>
2022-02-25 18:04:04 +0800

    添加个人页微信登录获取头像昵称操作

--- miniprogram/pages/user/user.js
+++ miniprogram/pages/user/user.js
@@ -1,66 +1,178 @@
 // pages/user/user.js
+import { DEFAULT_AVATAR_URL, DEFAULT_NICK_NAME } from "../../utils/constants";
+import { showOpenSettingModel } from "../../utils/commonUtils";
+import { getMultiOpenData, getOpenData } from "../../utils/cloudUtils.js";
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    avatarUrl: DEFAULT_AVATAR_URL,
+    nickName: DEFAULT_NICK_NAME,
+    userInfo: null,
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad() {
+    // wx.cloud.callFunction({
+    //   name: "testFunction",
+    // }).then(res => {
+    //   console.log(res);
+    // })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  // 获取头像和昵称
+  onWXLogin() {
+    const that = this;
+    wx.showLoading();
+    wx.getUserProfile({
+      desc: "获取您的头像和昵称",
+      success(res) {
+        // console.log(res);
+        wx.hideLoading();
+        const { avatarUrl, nickName } = res.userInfo;
+        that.setData({
+          avatarUrl,
+          nickName,
+          userInfo: res.userInfo,
+        })
+      },
+      fail(err) {
+        console.error(err);
+        wx.hideLoading();
+        wx.showToast({
+          title: "无法获取",
+          icon: "error",
+          duration: 2000,
+        });
+      },
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  // onGetPhoneNumber(e) {
+  //   console.log(e);
+  // },
 
-  },
+  // onChooseAvatar(e) {
+  //   console.log(e);
+  //   const { avatarUrl } = e.detail;
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
+  //   this.setData({
+  //     avatarUrl,
+  //   });
+  // },
 
-  },
+  // onLoad() {},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
+  // onAuthLocation() {
+  //   wx.authorize({
+  //     scope: "scope.userLocationBackground",
+  //     success(res) {
+  //       console.log(res);
+  //     },
+  //     fail(err) {
+  //       showOpenSettingModel();
+  //     },
+  //   });
+  //   // .then((res) => {
+  //   //   console.log(res);
+  //   // })
+  //   // .catch((err) => {
+  //   //   console.log(err);
+  //   // });
+  // },
 
-  },
+  // onOpenSetting(e) {
+  //   wx.openSetting({
+  //     withSubscriptions: true,
+  //     success(res) {
+  //       console.log(res);
+  //     },
+  //   });
+  // },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
+  // onConfirmNickName(e) {
+  //   // console.log(e);
+  //   const { value: nickName } = e.detail;
+  //   console.log(nickName);
+  //   this.setData({
+  //     nickName,
+  //   });
+  // },
 
-  },
+  // onGetUserProfile(e) {
+  //   const that = this;
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
+  //   // 获取头像和昵称
+  //   wx.getUserProfile({
+  //     desc: "获取你的头像和昵称",
+  //     success: function (res) {
+  //       console.log(res);
+  //       const { avatarUrl, nickName } = res.userInfo;
+  //       that.setData({
+  //         avatarUrl,
+  //         nickName,
+  //       });
+  //     },
+  //     fail: function (res) {
+  //       console.log(res);
+  //     },
+  //   });
 
-  },
+  //   // 直接返回匿名信息（无弹窗）
+  //   // wx.getUserInfo({
+  //   //   desc: '获取你的头像和昵称',
+  //   //   success: function(res) {
+  //   //     console.log(res);
+  //   //   },
+  //   //   fail: function(res) {
+  //   //     console.log(res);
+  //   //   }
+  //   // })
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  //   // 如需获取用户身份标识符只需要调用wx.login接口即可
+  //   // wx.login({
+  //   //   timeout: 2000,
+  //   //   success: function (res) {
+  //   //     console.log(res);
+  //   //   },
 
-  }
-})
+  //   //   fail: function (res) {
+  //   //     console.log(res);
+  //   //   },
+  //   // });
+  // },
+
+  // async onGetWXRun(e) {
+  //   const res2 = await wx.getUserProfile({
+  //     desc: "获取用户头像和昵称",
+  //   });
+  //   const res1 = await wx.getWeRunData();
+  //   // console.log(res1);
+  //   // console.log(res2);
+  //   getMultiOpenData({
+  //     userProfile: res2.cloudID,
+  //     weRunData: res1.cloudID,
+  //     userProfile1: res2.cloudID,
+  //     weRunData1: res1.cloudID,
+  //     userProfile2: res2.cloudID,
+  //     weRunData2: res1.cloudID,
+  //     userProfile3: res2.cloudID,
+  //     weRunData3: res1.cloudID,
+  //   }).then((res) => {
+  //     console.log(res);
+  //   });
+
+  //   getOpenData(res1.cloudID).then((res) => {
+  //     console.log(res);
+  //   });
+  // },
+
+  // onCheckSession() {
+  //   wx.checkSession({
+  //     success: (res) => {
+  //       console.log(res);
+  //     },
+  //     fail(err) {
+  //       console.log(err);
+  //     },
+  //   });
+  // },
+});
