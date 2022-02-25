0659bddccd5877c509294cf42b5a5b6cffbbe9d5
TCENT\bocheng.zhao <bocheng.zhao@ly.com>
2022-02-25 18:04:41 +0800

    添加常量文件

---
+++ miniprogram/utils/constants.js
@@ -0,0 +1,3 @@
+export const DEFAULT_AVATAR_URL = "https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0";
+
+export const DEFAULT_NICK_NAME = "请登录"
