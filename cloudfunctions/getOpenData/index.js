3ea377b4bb904dd19ebc628fb7a9e50c6a878d3b
TCENT\bocheng.zhao <bocheng.zhao@ly.com>
2022-02-25 14:01:52 +0800

    添加云环境初始化步骤

--- cloudfunctions/getOpenData/index.js
+++ cloudfunctions/getOpenData/index.js
@@ -1,5 +1,10 @@
 const Cloud = require("wx-server-sdk");
 
+// 非必要初始化操作，仅为防止本地没有云端初始化
+Cloud.init({
+  env: Cloud.DYNAMIC_CURRENT_ENV
+});
+
 exports.main = async (event, context) => {
   // return event[event.keyName];
   const res = Object.assign({}, event);
